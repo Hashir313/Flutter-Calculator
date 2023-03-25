7bdb72b78b451f714fbcf4b4518cb9f40c7b5603
Hashir Bin Masood <hashirmasood313@gmail.com>
2023-03-25 23:49:00 +0500

    Full version

---
+++ lib/ui/calculator_ui.dart
@@ -0,0 +1,160 @@
+// ignore_for_file: non_constant_identifier_names
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyCalculator extends StatefulWidget {
+  const MyCalculator({Key? key}) : super(key: key);
+
+  @override
+  State<MyCalculator> createState() => _MyCalculatorState();
+}
+
+class _MyCalculatorState extends State<MyCalculator> {
+  int? firstNum;
+  int? secondNum;
+  String? result;
+  String? equation;
+  String textToDisplay = "0";
+  String? operatorToPerform;
+
+  void buttonClicked(String buttonText) {
+    if (buttonText == "C") {
+      textToDisplay = "0";
+      firstNum = 0;
+      secondNum = 0;
+      result = "0";
+    }
+    else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "x" ||
+        buttonText == "÷") {
+      firstNum = int.parse(textToDisplay);
+      result = '0';
+      operatorToPerform = buttonText;
+    } else if (buttonText == "=") {
+      secondNum = int.parse(textToDisplay);
+      if (operatorToPerform == "+") {
+        result = (firstNum! + secondNum!).toString();
+      }
+      if (operatorToPerform == "-") {
+        result = (firstNum! - secondNum!).toString();
+      }
+      if (operatorToPerform == "x") {
+        result = (firstNum! * secondNum!).toString();
+      }
+      if (operatorToPerform == "÷") {
+        result = (firstNum! / secondNum!).toString();
+      }
+      if (operatorToPerform == "%") {
+
+      }
+    } else {
+      result = int.parse(textToDisplay + buttonText).toString();
+    }
+    setState(() {
+      textToDisplay = result!;
+    });
+  }
+
+  Widget CalculatorButton(
+      {required String buttonValue, required Color buttonColor}) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: TextButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(buttonColor),
+              shape: const MaterialStatePropertyAll(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50)))),
+            ),
+            onPressed: () {
+              buttonClicked(buttonValue);
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              child: Text(
+                buttonValue,
+                style: GoogleFonts.figtree(fontSize: 25.0, color: Colors.white),
+              ),
+            )),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: const Text('Hashir\'s Calculator'),
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        titleSpacing: 1.0,
+        titleTextStyle:
+            GoogleFonts.figtree(fontSize: 22.0, fontWeight: FontWeight.bold),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            alignment: Alignment.bottomRight,
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+            child: Text(
+              textToDisplay,
+              style: GoogleFonts.figtree(fontSize: 30.0, color: Colors.white),
+            ),
+          ),
+          Row(
+            children: [
+              CalculatorButton(
+                  buttonValue: '7', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: '8', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: '9', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: 'x', buttonColor: Colors.orange),
+            ],
+          ),
+          Row(
+            children: [
+              CalculatorButton(
+                  buttonValue: '4', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: '5', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: '6', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: '-', buttonColor: Colors.orange),
+            ],
+          ),
+          Row(
+            children: [
+              CalculatorButton(
+                  buttonValue: '1', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: '2', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: '3', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: '÷', buttonColor: Colors.orange),
+            ],
+          ),
+          Row(
+            children: [
+              CalculatorButton(
+                  buttonValue: 'C', buttonColor: Colors.orange),
+              CalculatorButton(
+                  buttonValue: '0', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: '00', buttonColor: Colors.grey.shade600),
+              CalculatorButton(
+                  buttonValue: '=', buttonColor: Colors.orange),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
